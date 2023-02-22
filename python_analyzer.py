7350e6c66bd00ed3410e2c7c81f07da772fb7467
tsaiba <keutel@campus.tu-berlin.de>
2023-02-23 00:19:49 +0100

    Clean up analyzers

--- python_analyzer.py
+++ python_analyzer.py
@@ -1,77 +1,9 @@
 # Generally DEPRECIATED
-import re
 
 from tree_sitter import Language, Tree, Node
 
 from extractor import print_children, traverse_sub_tree
-from config import keywords, PythonNodeNames
 from python_extractor import PythonExtractor
-
-
-# Copied from Python Extractor
-def check_parent(node: Node, param_vec: dict):
-    parent = node
-    while parent.parent:
-        parent = parent.parent
-        if parent.type == "block":
-            break
-    if parent.type != "module":
-        assert parent.type == "block"
-        if parent.parent.type == "if_statement":
-            param_vec["inside_if"] = True
-        elif parent.parent.type == "elif_clause":
-            param_vec["inside_elif"] = True
-        elif parent.parent.type == "else_clause":
-            if parent.parent.parent.type == "if_statement":
-                param_vec["inside_if_else"] = True
-            elif parent.parent.parent.type == "try_statement":
-                param_vec["inside_try_else"] = True
-            # For..else, While..else,..
-        elif parent.parent.type == "try_statement":
-            param_vec["inside_try"] = True
-        elif parent.parent.type == "except_clause":
-            param_vec["inside_except"] = True
-        elif parent.parent.type == "finally_clause":
-            param_vec["inside_finally"] = True
-
-
-def check_block(node: Node, param_vec: dict, args, keyword: str):
-    for child in node.children:
-        if not args.debug and False not in param_vec.values():
-            return
-        if not param_vec["contains_logging"] and child.type == "expression_statement":
-            # assert len(child.children) == 1 # NOT ALWAYS TRUE
-            if len(child.children) != 1:
-                # print("Expression statement with more than one child found:")
-                # print(f"Line {child.start_point[0] + 1}")
-                # print(child.text.decode("UTF-8"))
-                # print(child.children)
-                for exp_child in child.children:
-                    if exp_child.type == "call":
-                        if re.search(keyword, exp_child.text.decode("UTF-8")):
-                            param_vec["contains_logging"] = True
-            elif child.children[0].type == "call":
-                if re.search(keyword, child.children[0].text.decode("UTF-8")):
-                    param_vec["contains_logging"] = True
-        elif not param_vec["contains_try"] and child.type == "try_statement":
-            param_vec["contains_try"] = True
-        elif not param_vec["contains_if"] and child.type == "if_statement":
-            param_vec["contains_if"] = True
-        elif not param_vec["contains_with"] and child.type == "with_statement":
-            param_vec["contains_with"] = True
-        # More checks for expanded dict
-
-
-def check_if(node: Node, param_vec: dict, args, keyword: str):
-    check_block(node.child_by_field_name("consequence"), param_vec, args, keyword)
-
-
-def check_try(node: Node, param_vec: dict, args, keyword: str):
-    check_block(node.child_by_field_name("body"), param_vec, args, keyword)
-
-
-def check_def(node: Node, param_vec: dict, args, keyword: str):
-    check_block(node.child_by_field_name("body"), param_vec, args, keyword)
 
 
 class PythonAnalyzer(PythonExtractor):
@@ -86,7 +18,7 @@ class PythonAnalyzer(PythonExtractor):
         # Name of the Python logging module
         self.keyword = "logg(ing|er)"
 
-    def analyze(self) -> list:
+    def analyze(self):
         """ Starts the analyses """
         print_children(self.tree.root_node, print_unnamed=self.settings.debug);
 
@@ -104,74 +36,17 @@ class PythonAnalyzer(PythonExtractor):
                 # print("#" * 80)
                 # print(node.text.decode("UTF-8"))
                 # exit()
-            # return []
-        # recommendations = []
-        # return recommendations
 
-    def proto_context(self):
-        # print_children(self.tree.root_node)
-        function_definiton_query = self.lang.query("(function_definition) @funcdef")
-        function_definiton_nodes = function_definiton_query.captures(self.tree.root_node)
-        block_query = self.lang.query("(block) @block")
-        # block_nodes = block_query.captures(self.tree.root_node)
-        # prev = None
-        for funcdef_node, tag in function_definiton_nodes:
-            funcdef_node: Node
-            block_nodes = block_query.captures(funcdef_node)
-            print("#" * 80)
-            print(funcdef_node)
-            print(funcdef_node.text.decode("UTF-8"))
-            print("+" * 80)
-            for block_node, tag in block_nodes:
-                print(block_node)
-                print(block_node.text.decode("UTF-8"))
-                print("-" * 80)
-                print("Ast path from function definition to block:")
-                pathlist = []
-                wandering_node = block_node
-                while wandering_node != funcdef_node:
-                    pathlist.append(wandering_node)
-                    wandering_node = wandering_node.parent
-                pathlist.reverse()
-                for node in pathlist:
-                    if node.is_named and node.type in PythonNodeNames.most_node_types:
-                        print(node.type)
-                print("-" * 80)
-                print("Previous nodes in function definition:")
 
-                # last_node = None
-                def check_and_print(node: Node):
-                    if node.is_named and node.type in PythonNodeNames.most_node_types:
-                        if node.type == "call" and keyword.match(node.text.decode("UTF-8").lower()):
-                            # print("Logging call found")
-                            return
-                        print(node.type)
 
-                for node in traverse_sub_tree(funcdef_node, block_node):
-                    if node.type != "function_definition":
-                        check_and_print(node)
-                        # print(node)
-                        # print(node.type)
-                        # last_node = node
-                        # print("-" * 80)
-                # if last_node:
-                #     for node2 in traverse_sub_tree(last_node):
-                #         if node2.is_named and node2.type in visible_node_types:
-                #             print(node2.type)
-                for node in traverse_sub_tree(block_node):
-                    check_and_print(node)
-                print("=" * 80)
-
-    def get_all_named_children_with_parent_of_type(self, node_type: str):
-        query = self.lang.query("(" + node_type + " (_) @inner)")
-        nodes = query.captures(self.tree.root_node)
-        for node in nodes:
-            print(node)
 
     def check_for_module_import(self) -> bool:
         """
+        ############
         DEPRECATED: Detecting the logging framework is out of scope.
-        The user will have to supply the name of the logging framework.
+        Also DEPRECATED: The user will have to supply the name of the logging framework.
+        We now use a regex built to detect logging statements from commonly used logging frameworks
+        but this functionality may prove usesful again at some point.
         ############
         Checks if the logging module is imported.
         Also finds alias of logging module if used and updates self.keyword accordingly
@@ -200,6 +75,8 @@ class PythonAnalyzer(PythonExtractor):
 
     def exception_handling_via_treesitter(self):
         """
+        DEPRECATED: This was the initial Tree-sitter usage
+
         Checks for logging in the direct children function calls of the exception nodes of the ast,
         e.g. logging inside an if-statement in the exception handling is not
         accepted as it is not guaranteed to be reached.
@@ -239,6 +116,8 @@ class PythonAnalyzer(PythonExtractor):
                          f"out of {len(all_exceptions)} exception handling blocks.")
 
     def ts_example(self):
+        """Example of Tree-sitter usage"""
+
         # Recursively print the whole ast in stdout
         print("Abstract syntax tree of ts_py_example.py. Start and end points of nodes are 0-indexed.")
         print("Below the ast are the non-import logging occurrences and their logical parent's ast subtree.")
